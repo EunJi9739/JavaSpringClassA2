0f1e5c5e25f5a4429a3942a343ebbdc2bee6a134
EunJi9739 <h3king27@gmail.com>
2023-06-07 17:28:31 +0900

    TDD 추가

--- board/src/test/java/org/koreait/tests/BoardApiSaveTest.java
+++ board/src/test/java/org/koreait/tests/BoardApiSaveTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.charset.Charset;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,15 +23,61 @@ public class BoardApiSaveTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+    private String getParams(String subject, String content) {
+        //{"subject" : "테스트 제목", "content" : "테스트 내용"}
+        String params = String.format("{\"subject\":\"%s\",\"content\":\"%s\"}", subject, content);
+
+        return params;
+    }
+
     @Test
     @DisplayName("게시글 작성 성공시 응답코드 201")
     void saveSuccessTest() throws Exception {
-        String params = String.format("{\"subject\":\"%s\",\"content\":\"%s\"}","테스트 제목","테스트 내용");
+
+        String params = getParams("테스트 제목!", "테스트 내용!");
 
         mockMvc.perform(post("/api/board/write")
-                .contentType("application/json")
-                .content(params))
+                        .contentType("application/json")
+                        .content(params))
                 .andDo(print())
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("필수 항목(제목) 누락시 발생 문구 테스트")
+    void requiredSubjectTest() throws Exception {
+        String params = getParams("", "테스트 내용");
+
+        String body = mockMvc.perform(post("/api/board/write")
+                                .contentType("application/json")
+                                .content(params))
+                                .andReturn()
+                                .getResponse()
+                                .getContentAsString(Charset.forName("UTF-8")); //응답 Body 데이터
+
+        //System.out.println(body);
+        assertTrue(body.contains("제목을 입력"));
+
+
+    }
+
+    @Test
+    @DisplayName("필수 항목(내용) 누락시 발생 문구 테스트")
+    void requiredContentTest() throws Exception {
+        String params = getParams("테스트 제목", "");
+
+        String body = mockMvc.perform(post("/api/board/write")
+                        .contentType("application/json")
+                        .content(params))
+                .andReturn()
+                .getResponse()
+                .getContentAsString(Charset.forName("UTF-8")); //응답 Body 데이터
+
+        assertTrue(body.contains("내용을 입력"));
+
+        System.out.println(body);
+
+
     }
 }
