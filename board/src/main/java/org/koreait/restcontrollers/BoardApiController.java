46757eab73827c5aea4a2488eff6ed9faf45cb95
EunJi9739 <h3king27@gmail.com>
2023-06-08 12:41:43 +0900

    통합구현 시험 연습

--- board/src/main/java/org/koreait/restcontrollers/BoardApiController.java
+++ board/src/main/java/org/koreait/restcontrollers/BoardApiController.java
@@ -5,11 +5,12 @@ import lombok.extern.java.Log;
 
 import org.koreait.controllers.BoardForm;
 import org.koreait.entities.BoardData;
-import org.koreait.models.board.BoardSaveService;
+import org.koreait.models.board.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @Log
@@ -19,12 +20,50 @@ import org.springframework.web.bind.annotation.*;
 public class BoardApiController {
 
     private final BoardSaveService saveService;
+    private final BoardListService listService;
+    private final BoardInfoService infoService;
+    private final BoardDeleteService deleteService;
 
     @PostMapping("/write")
-    public ResponseEntity<Object> write(@RequestBody BoardForm boardForm){
+    public ResponseEntity<Object> write(@RequestBody BoardForm boardForm) {
         //log.info(boardData.toString());
         saveService.save(boardForm);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<BoardData>> list() {
+        List<BoardData> items = listService.gets();
+        if (items.isEmpty()) {
+            throw new BoardValidationException("조회에 실패했습니다.");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(items);
+
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Object> get(@PathVariable Long id) {
+        BoardData boardData = infoService.get(id);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(boardData);
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id){
+        deleteService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Object> write(@PathVariable Long id, @RequestBody BoardForm boardForm){
+        boardForm.setId(id);
+        boardForm.setMode("update");
+        saveService.save(boardForm);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+
+    }
+
 }
