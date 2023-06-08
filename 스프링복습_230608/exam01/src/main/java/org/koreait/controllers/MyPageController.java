87c57c9f89064f8fa12e5ce8cc617d4f80d879e1
EunJi9739 <h3king27@gmail.com>
2023-06-08 14:36:49 +0900

    소스 파일 업로드

--- 스프링복습_230608/exam01/src/main/java/org/koreait/controllers/MyPageController.java
+++ 스프링복습_230608/exam01/src/main/java/org/koreait/controllers/MyPageController.java
@@ -1,5 +1,6 @@
 package org.koreait.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mypage")
 public class MyPageController {
     @GetMapping
-    public String index(){
-
+    public String index(HttpServletRequest request){
+        String commonData=(String) request.getAttribute("commonData");
+        System.out.println(commonData);
         return "mypage";
     }
 }
